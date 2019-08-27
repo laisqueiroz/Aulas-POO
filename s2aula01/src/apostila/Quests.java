13424736bff244905e5e1ef26ebaf26b1f3416b3
laisqueiroz <lais.frequeiroz@gmail.com>
2019-08-27 09:11:48 -0300

    exercicios

---
+++ s2aula01/src/apostila/Quests.java
@@ -0,0 +1,12 @@
+package apostila;
+
+public class Quests {
+	public static void main(String[] args) {
+		//01
+		int i;
+		int y = 301;
+		for (i = 150; i < y ; i++) {
+			System.out.println(i);
+		}
+	}
+}
